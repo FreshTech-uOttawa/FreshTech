9a21fddf5aac563cc9726c35518e74ef92cadcbf
8x5y1a <jeff.gamache@icloud.com>
2025-10-07 16:56:53 -0400

    Added graph image
    - Added graph image
    - Added some notes to resolve the weird/ugly animation that I made with the fridge.

--- simulation/simulation.js
+++ simulation/simulation.js
@@ -3,6 +3,13 @@ const screen = document.querySelector(".screen");
 const beginBtn = document.querySelector(".begin-simulation-btn");
 
 function beginSimulation() {
+  //TODO: Probably make it quick and subtle
+  // It should take away the focus from the background and the user shouldnt directly noticed the background fade
+  // Could see the fridge all the time and just zoom on start
+  // Could make the stars go to the left and make the fridge appear from the right
+  // Could make the stars go forward and grow the fridge from the middle? (It should be quick)
+  // Could make a really bright star in the middle and it will look like we are going to that star when going forward
+  // and the star will be the fridge? ** Seems good, but hard
   beginBtn.style.display = "none";
   fridgeContainer.classList.add("fridge-animation");
   setTimeout(() => {
